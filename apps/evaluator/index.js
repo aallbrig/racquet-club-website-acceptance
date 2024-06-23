31004724c47111df9ecb0f83c23db8c3336d0dbc
Andrew Allbright <aallbrig@gmail.com>
2024-06-23 15:15:44 -0400

    Recursively crawl through website and generate 'report' (just JSON for now)

--- apps/evaluator/index.js
+++ apps/evaluator/index.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 import validator from 'validator';
 
-async function evaluate(targetUrl) {
+async function generateReport(targetUrl) {
   const report = {
     visited: []
   };
@@ -12,10 +12,7 @@ async function evaluate(targetUrl) {
       slowMo: 250
     });
     const page = await browser.newPage();
-    await page.goto(targetUrl);
-    if (report.visited.indexOf(targetUrl) === -1) {
-      report.visited.push(targetUrl);
-    }
+    await crawl(targetUrl, page, report);
   } catch (error) {
     console.error(`Failed to evaluate ${targetUrl}: ${error}`);
     process.exit(1);
@@ -25,6 +22,51 @@ async function evaluate(targetUrl) {
     }
   }
   return report;
+}
+
+async function crawl(targetUrl, page, report, additionalTasks = []) {
+  const targetUrlObj = new URL(targetUrl);
+  if (report.visited.includes(targetUrlObj.href)) {
+    return;
+  }
+
+  report.visited.push(targetUrlObj.href);
+  report[targetUrlObj.href] = {};
+  await page.goto(targetUrlObj.href, { waitUntil: 'networkidle0' });
+  report[targetUrlObj.href].title = await page.title();
+  report[targetUrlObj.href].internalLinks = [];
+  report[targetUrlObj.href].externalLinks = [];
+
+  const links = await page.$$eval('a', as => as.map(a => a.href));
+  const linkUrls = links.map(link => {
+    const linkUrl = new URL(link);
+    linkUrl.hash = ""; // removes hash so that example.com/ and example.com/# are considered the same
+    return linkUrl;
+  });
+
+  const uniqueInternalLinks = new Set();
+  const uniqueExternalLinks = new Set();
+
+  for (const linkUrl of linkUrls) {
+    if (linkUrl.hostname === targetUrlObj.hostname) {
+      uniqueInternalLinks.add(linkUrl.href);
+    } else {
+      uniqueExternalLinks.add(linkUrl.href);
+    }
+  }
+
+  report[targetUrlObj.href].internalLinks = Array.from(uniqueInternalLinks)
+    .map(href => new URL(href))
+    .sort((a, b) => a.href.localeCompare(b.href));
+  report[targetUrlObj.href].externalLinks = Array.from(uniqueExternalLinks)
+    .map(href => new URL(href))
+    .sort((a, b) => a.href.localeCompare(b.href));
+
+  additionalTasks.forEach(additionalTask => additionalTask(page));
+
+  for (const link of report[targetUrlObj.href].internalLinks) {
+    await crawl(link.href, page, report);
+  }
 }
 
 function usage() {
@@ -52,9 +94,10 @@ function main() {
     usage();
     process.exit(1);
   }
-  const reportPromise = evaluate(targetUrl);
+  const reportPromise = generateReport(new URL(targetUrl));
   reportPromise.then((report) => {
-    console.log(report);
+    console.log(JSON.stringify(report, null, 2));
+    process.exit(0);
   });
 }
 
